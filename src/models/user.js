975384bec36de62be46110029e9caf32a6349793
Juan Echeverria <juan.echeverriatro@ug.edu.ec>
2024-08-24 23:07:28 -0500

    Implementacion de tokens

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const db = require('../config/db');
+const bcrypt = require('bcryptjs');
 
 const User = function(user) {
     this.name = user.name;
@@ -39,39 +40,51 @@ User.findById = (id, result) => {
 };
 
 User.create = (newUser, result) => {
-    db.query("INSERT INTO users (name, usercode, password) values (?,?,?)",
-        [newUser.name, newUser.usercode, newUser.password], (err, res) => {
+    bcrypt.hash(newUser.password, 10, (err, hashedPassword) => {
         if (err) {
-            console.log("error: ", err);
             result(err, null);
             return;
         }
-        console.log("created user: ", { id: res.insertId, ...newUser });
-        result(null, { id: res.insertId, ...newUser });
-    });
-};
-
-User.updateById = (id, user, result) => {
-    db.query(
-        "UPDATE users SET name = ?, usercode = ?, password = ? WHERE id_user = ?",
-        [user.name, user.usercode, user.password, id],
-        (err, res) => {
+        db.query("INSERT INTO users (name, usercode, password) values (?,?,?)",
+            [newUser.name, newUser.usercode, hashedPassword], (err, res) => {
             if (err) {
                 console.log("error: ", err);
                 result(err, null);
                 return;
             }
+            console.log("created user: ", { id: res.insertId, ...newUser });
+            result(null, { id: res.insertId, ...newUser });
+        });
+    });
+};
 
-            if (res.affectedRows === 0) {
-                // Not found User with the id
-                result({ kind: "not_found" }, null);
-                return;
-            }
-
-            console.log("updated user: ", { id: id, ...user });
-            result(null, { id: id, ...user });
+User.updateById = (id, user, result) => {
+    bcrypt.hash(user.password, 10, (err, hashedPassword) => {
+        if (err) {
+            result(err, null);
+            return;
         }
-    );
+        db.query(
+            "UPDATE users SET name = ?, usercode = ?, password = ? WHERE id_user = ?",
+            [user.name, user.usercode, hashedPassword, id],
+            (err, res) => {
+                if (err) {
+                    console.log("error: ", err);
+                    result(err, null);
+                    return;
+                }
+    
+                if (res.affectedRows === 0) {
+                    // Not found User with the id
+                    result({ kind: "not_found" }, null);
+                    return;
+                }
+    
+                console.log("updated user: ", { id: id, ...user });
+                result(null, { id: id, ...user });
+            }
+        );
+    });
 };
 
 module.exports = User;
